505afe2c28591bfd6c893449f9e309cfcb980c77
Patric Stout <truebrain@openttd.org>
2018-07-21 16:21:06 +0200

    Add: also show when jobs start/stop

--- dorpsgek_runner/commands/job.py
+++ dorpsgek_runner/commands/job.py
@@ -1,12 +1,17 @@
+import logging
 import os
 import tempfile
 
 from dorpsgek_runner import config
 from dorpsgek_runner import runner
 
+log = logging.getLogger(__name__)
+
 
 @runner.register("job.start")
 async def job_start(event, ws):
+    log.info("Starting with a new job")
+
     ws.tempdir = tempfile.TemporaryDirectory("", "runner.", config.WORKING_FOLDER)
     os.mkdir(f"{ws.tempdir.name}/artifact")
 
@@ -15,3 +20,5 @@ async def job_start(event, ws):
 async def job_done(event, ws):
     ws.tempdir.cleanup()
     ws.tempdir = None
+
+    log.info("All done with the requested job")
